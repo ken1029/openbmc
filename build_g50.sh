acb80a327acf21790005922635e895a6bf0a1b09
RNG <rednewguy@gmail.com>
2017-05-15 13:32:08 -0700

    Updated build script
    Signed-off-by: RNG <rednewguy@gmail.com>

--- build_g50.sh
+++ build_g50.sh
@@ -1,7 +1,20 @@
-#!/bin/bash
+#!/bin/bash -x
 
 REPO_DIR=`pwd`
-TEMPLATECONF=$REPO_DIR/meta-openbmc-machines/meta-microsoft/meta-g50/conf/
-source $REPO_DIR/oe-init-build-env
-touch conf/sanity.conf
+
+echo "
+Checking if build directory ../build_dir exists..."
+if [ ! -d ../build_dir ]; then
+    echo "No build directory. Creating ../build_dir"
+    mkdir ../build_dir
+else
+    echo "Directory ../build_dir already exists"
+fi
+
+cd $REPO_DIR/../build_dir
+
+echo "
+Set the conf file for build and run config"
+TEMPLATECONF=$REPO_DIR/meta-openbmc-machines/meta-microsoft/meta-g50/conf source $REPO_DIR/oe-init-build-env
 bitbake obmc-phosphor-image
+
